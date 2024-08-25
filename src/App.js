00cff68c7bdd7d0ad51c8e0902d9119a85ff2867
Ivavi17 <avi17803@gmail.com>
2024-08-25 13:04:30 +0530

    ghj

--- src/App.js
+++ src/App.js
@@ -1,92 +1,3 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// function App() {
-//   const [input, setInput] = useState('');
-//   const [response, setResponse] = useState(null);
-//   const [selectedOptions, setSelectedOptions] = useState([]);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const parsedData = JSON.parse(input);
-//       const res = await axios.post('https://holistic-viridian-periwinkle.glitch.me/bfhl', parsedData, {
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       });
-//       setResponse(res.data);
-//     } catch (error) {
-//       console.error('Error submitting data:', error);
-//       setResponse({ is_success: false, message: 'Failed to submit data' });
-//     }
-//   };
-
-//   const handleSelectChange = (e) => {
-//     const options = Array.from(e.target.selectedOptions, option => option.value);
-//     setSelectedOptions(options);
-//   };
-
-//   const renderResponse = () => {
-//     if (!response) return null;
-
-//     const { numbers, alphabets, highest_lowercase_alphabet } = response;
-
-//     return (
-//       <div>
-//         {selectedOptions.includes('Numbers') && (
-//           <div>
-//             <h3>Numbers:</h3>
-//             <pre>{JSON.stringify(numbers, null, 2)}</pre>
-//           </div>
-//         )}
-//         {selectedOptions.includes('Alphabets') && (
-//           <div>
-//             <h3>Alphabets:</h3>
-//             <pre>{JSON.stringify(alphabets, null, 2)}</pre>
-//           </div>
-//         )}
-//         {selectedOptions.includes('Highest lowercase alphabet') && (
-//           <div>
-//             <h3>Highest Lowercase Alphabet:</h3>
-//             <pre>{JSON.stringify(highest_lowercase_alphabet, null, 2)}</pre>
-//           </div>
-//         )}
-//       </div>
-//     );
-//   };
-
-//   return (
-//     <div>
-//       <h1>BFHL Frontend</h1>
-//       <form onSubmit={handleSubmit}>
-//         <textarea
-//           value={input}
-//           onChange={(e) => setInput(e.target.value)}
-//           placeholder='Enter JSON here...'
-//           rows="10"
-//           cols="50"
-//         />
-//         <br />
-//         <button type="submit">Submit</button>
-//       </form>
-
-//       <div>
-//         <h3>Select what to display:</h3>
-//         <select multiple={true} onChange={handleSelectChange}>
-//           <option value="Numbers">Numbers</option>
-//           <option value="Alphabets">Alphabets</option>
-//           <option value="Highest lowercase alphabet">Highest lowercase alphabet</option>
-//         </select>
-//       </div>
-
-//       {renderResponse()}
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -123,22 +34,23 @@ function App() {
 
     return (
       <div>
-        {selectedOptions.includes('Numbers') && numbers && (
+        <h3>Filtered Response</h3>
+        {selectedOptions.includes('Numbers') && numbers.length > 0 && (
           <div>
-            <h3>Numbers:</h3>
-            <pre>{JSON.stringify(numbers, null, 2)}</pre>
+            <h4>Numbers:</h4>
+            <pre>{numbers.join(', ')}</pre>
           </div>
         )}
-        {selectedOptions.includes('Alphabets') && alphabets && (
+        {selectedOptions.includes('Alphabets') && alphabets.length > 0 && (
           <div>
-            <h3>Alphabets:</h3>
-            <pre>{JSON.stringify(alphabets, null, 2)}</pre>
+            <h4>Alphabets:</h4>
+            <pre>{alphabets.join(', ')}</pre>
           </div>
         )}
-        {selectedOptions.includes('Highest lowercase alphabet') && highest_lowercase_alphabet && (
+        {selectedOptions.includes('Highest lowercase alphabet') && highest_lowercase_alphabet.length > 0 && (
           <div>
-            <h3>Highest Lowercase Alphabet:</h3>
-            <pre>{JSON.stringify(highest_lowercase_alphabet, null, 2)}</pre>
+            <h4>Highest Lowercase Alphabet:</h4>
+            <pre>{highest_lowercase_alphabet.join(', ')}</pre>
           </div>
         )}
       </div>
@@ -153,7 +65,7 @@ function App() {
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder='Enter JSON here...'
-          rows="10"
+          rows="5"
           cols="50"
         />
         <br />
@@ -161,7 +73,7 @@ function App() {
       </form>
 
       <div>
-        <h3>Select what to display:</h3>
+        <h3>Multi Filter</h3>
         <select multiple={true} onChange={handleSelectChange}>
           <option value="Numbers">Numbers</option>
           <option value="Alphabets">Alphabets</option>
